214f7fac945b3585d48aab9e13f167db8f7cb31d
Yosuf Gurung <yosufgurung@gmail.com>
2019-01-15 00:32:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,36 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+
+  let toAdd = {
+    'itemName': item,
+    'itemPrice': Math.floor(Math.random()*100)
+  }
+  cart.push(toAdd)
+
+  return `${item} has been added to your cart.`
+
+}
+
+function cartItem(itemName) {
+  itemName = item
+
+  for (let i = 0; i < itemName.length; i++) {
+    return {
+      itemName: itemName,
+      itemPrice: price(1, 100)
+    }
+  }
+}
+
+function price(min, max) {
+  // min = 0
+  // max = 101
+  return math.floor(math.random() * (max - min)) + min;
 }
 
 function viewCart() {
-  // write your code here
+
 }
 
 function total() {
